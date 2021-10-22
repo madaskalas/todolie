747ad2ae1ca4a574fa8927748deec679f49a14c6
25797700+madaskalas@users.noreply.github.com <25797700+madaskalas@users.noreply.github.com>
2021-10-22 14:00:26 +0300

    Change state of checked box

--- main.js
+++ main.js
@@ -2,14 +2,9 @@ console.log("This works!");
 
 function tasklist() {
     let task = document.getElementById("userinput").value;
+    let todolist = document.getElementById("todolist");
+
     console.log(`User typed: ${task}`);
-    /*
-    let ul = document.createElement("ul");
-    document.getElementById("todolist").appendChild(ul);
-    let li = document.createElement("li");
-    ul.appendChild(li);
-    li.innerHTML = task;
-    */
     document.getElementById("userinput").value = "";
     document.getElementById("container").style.display = "block";
 
@@ -25,14 +20,29 @@ function tasklist() {
     // Assign label
     let label = document.createElement("label");
     label.htmlFor = "checkid";
+    label.id = "labelid";
 
     // Append child
-    label.appendChild(document.createTextNode("Test Node"));
+    label.appendChild(document.createTextNode(task));
 
     todolist.appendChild(input);
     todolist.appendChild(label);
 
+    let br = document.createElement("br");
+    todolist.appendChild(br);
+
+    const cb = document.querySelector("#checkid");
+    cb.onclick = () => {
+        const res = cb.checked;
+        if (res) {
+            document.getElementById("labelid").style.textDecoration = "line-through";
+        }
+        else {
+            document.getElementById("labelid").style.textDecoration = "none";
+        }
+    };
 }
+
 function reset() {
     location.reload();
 }
