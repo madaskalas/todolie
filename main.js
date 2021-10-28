847cf3f3dcb7187cded1d55dae395660bad9af36
25797700+madaskalas@users.noreply.github.com <25797700+madaskalas@users.noreply.github.com>
2021-10-28 14:14:37 +0300

    Add Logo and favicon

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let arrList = [];
 
 function tasklist() {
     let task = document.getElementById("userinput").value;
-    let todolist = document.getElementById("todolist");
+    let todolist = document.querySelector(".todolist");
 
     document.getElementById("userinput").value = "";
     document.getElementById("container").style.display = "block";
@@ -15,11 +15,13 @@ function tasklist() {
     input.name = "checklist";
     input.value = "item";
     input.id = "checkid";
+    input.className = "checkclass";
 
     // Assign label
     let label = document.createElement("label");
     label.htmlFor = "checkid";
     label.id = "labelid";
+    label.className = "labelclass";
 
     // Append child
     label.appendChild(document.createTextNode(task));
@@ -30,52 +32,24 @@ function tasklist() {
     let br = document.createElement("br");
     todolist.appendChild(br);
 
-    const cb = document.querySelector("#checkid");
+    const cb = document.querySelector(".checkclass");
     cb.onclick = () => {
         const res = cb.checked;
         if (res) {
-            document.getElementById("labelid").style.textDecoration = "line-through";
-            
-            // Push into todo checked items
+            document.querySelector(".labelclass").style.textDecoration = "line-through";
+            // Push into todo querySelector items
             arrList.push(task);
             console.log(arrList);
-
-            // Create checkbox element
-            let input = document.createElement("input");
-
-            // Assign attributes
-            input.type = "checkbox";
-            input.name = "checklistdone";
-            input.value = "itemdone";
-            input.id = "checkiddone";
-
-            // Assign label
-            let label = document.createElement("label");
-            label.htmlFor = "checkiddone";
-            label.id = "labeliddone";
-
-            // Append child
-            label.appendChild(document.createTextNode(task));
-
-            donelist.appendChild(input);
-            donelist.appendChild(label);
-
-            let br = document.createElement("br");
-            donelist.appendChild(br);
-
-            document.getElementById("labelid").style.textDecoration = "line-through";
         }
         else {
-            document.getElementById("labelid").style.textDecoration = "none";
+            document.querySelector(".labelclass").style.textDecoration = "none";
         }
     };
 }
-
 function storeList() {
     arrList.push(task);
     console.log(arrList);
 }
-
 function reset() {
     location.reload();
 }
