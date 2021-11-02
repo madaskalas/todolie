039cc8e9ca5dcda32dca5ea6255cdcda21a419b9
25797700+madaskalas@users.noreply.github.com <25797700+madaskalas@users.noreply.github.com>
2021-11-02 20:12:19 +0200

    Add message when fields empty

--- main.js
+++ main.js
@@ -10,103 +10,96 @@ function tasklist() {
     let namelist = document.getElementById("namelist").value;
     console.log(`Name of list: ${namelist}`);
 
-    if (namelist == "") {
-        document.getElementById("warning1").innerHTML = "Please give a name for the list.";
-        document.getElementById("warning1").style.display = "block";
-    }
-    else {
-        document.getElementById("warning1").style.display = "none";
-        document.getElementById("namelist").disabled = true;
-    }
-
     let todolistname = document.querySelector(".todo").innerHTML = namelist;
 
-    if (task == "") {
-        document.getElementById("warning").innerHTML = "Please give a task!";
+    if (task == "" || namelist == "") {
+        document.getElementById("warning").innerHTML = "Please complete both fields!";
         document.getElementById("warning").style.display = "block";
+        
     }
     else {
         document.getElementById("warning").style.display = "none";
-    // Select DIV
-    let todolist = document.querySelector(".todolist");
+        document.getElementById("namelist").disabled = true;
+        // Select DIV
+        let todolist = document.querySelector(".todolist");
 
-    // Clears text input
-    document.getElementById("userinput").value = "";
+        // Clears text input
+        document.getElementById("userinput").value = "";
 
-    // Shows container
-    document.getElementById("containertodo").style.display = "block";
+        // Shows container
+        document.getElementById("containertodo").style.display = "block";
 
-    // Create input element
-    let input = document.createElement("input");
+        // Create input element
+        let input = document.createElement("input");
 
-    // Generates a random number
-    let random = Math.floor(Math.random() * 1000);
-    let randomlabel = Math.floor(Math.random() * 1001 + 1000);
-    let randombr = Math.floor(Math.random() * 100);
+        // Generates a random number
+        let random = Math.floor(Math.random() * 1000);
+        let randomlabel = Math.floor(Math.random() * 1001 + 1000);
+        let randombr = Math.floor(Math.random() * 100);
 
-    // Assign attributes
-    input.type = "checkbox";
-    input.name = "checklist";
-    input.value = task;
-    input.id = random;
-    input.className = "checkclass";
+        // Assign attributes
+        input.type = "checkbox";
+        input.name = "checklist";
+        input.value = task;
+        input.id = random;
+        input.className = "checkclass";
 
-    // Create label element
-    let label = document.createElement("label");
+        // Create label element
+        let label = document.createElement("label");
 
-    // Assign label
-    label.htmlFor = random;
-    label.id = randomlabel;
-    label.className = "labelclass";
+        // Assign label
+        label.htmlFor = random;
+        label.id = randomlabel;
+        label.className = "labelclass";
 
-    // Append childs
-    label.appendChild(document.createTextNode(task));
-    todolist.appendChild(input);
-    todolist.appendChild(label);
+        // Append childs
+        label.appendChild(document.createTextNode(task));
+        todolist.appendChild(input);
+        todolist.appendChild(label);
 
-    // Create br element
-    let br = document.createElement("br");
-    br.id = randombr;
+        // Create br element
+        let br = document.createElement("br");
+        br.id = randombr;
 
-    // Append child
-    todolist.appendChild(br);
+        // Append child
+        todolist.appendChild(br);
 
-    // Push to array
-    arrList.push(task);
-    console.log(arrList);
+        // Push to array
+        arrList.push(task);
+        console.log(arrList);
 
-    let t1 = document.querySelectorAll(".checkclass");
-    let t2 = document.querySelectorAll(".labelclass");
-    let t3 = document.getElementById(randombr);
-    console.log(t1);
-    console.log(t2);
+        let t1 = document.querySelectorAll(".checkclass");
+        let t2 = document.querySelectorAll(".labelclass");
+        let t3 = document.getElementById(randombr);
+        console.log(t1);
+        console.log(t2);
 
-    for (let i = 0; i <= arrList.length - 1; i++) {
-        console.log(`Before onclick: ${i}`);
-        // console.log(t1[i]);
-        t1[i].onclick = () => {
-            if (t1[i].checked) {
-                console.log(`Checked ${i}: ${t1[i].value}`);
-                t2[i].style.textDecoration = "line-through";
-                t2[i].style.backgroundColor = "lightgreen";
-                // t1[i].parentElement.removeChild(t1[i]);
-                // t2[i].parentElement.removeChild(t2[i]);
-                // t3.parentElement.removeChild(t3);
-                arrListDone.push(arrList[i]);
-                console.log(`Push to arrListDone`);
-                console.log(arrListDone);
-                console.log(`EndIf: ${i}`);
-            }
-            else {
-                console.log(`Not Checked ${i}: ${t1[i].value}`);
-                t2[i].style.textDecoration = "none";
-                t2[i].style.backgroundColor = "lightgray";
-                arrListDone.splice(i, 1);
-                console.log(arrListDone);
+        for (let i = 0; i <= arrList.length - 1; i++) {
+            console.log(`Before onclick: ${i}`);
+            // console.log(t1[i]);
+            t1[i].onclick = () => {
+                if (t1[i].checked) {
+                    console.log(`Checked ${i}: ${t1[i].value}`);
+                    t2[i].style.textDecoration = "line-through";
+                    t2[i].style.backgroundColor = "lightgreen";
+                    // t1[i].parentElement.removeChild(t1[i]);
+                    // t2[i].parentElement.removeChild(t2[i]);
+                    // t3.parentElement.removeChild(t3);
+                    arrListDone.push(arrList[i]);
+                    console.log(`Push to arrListDone`);
+                    console.log(arrListDone);
+                    console.log(`EndIf: ${i}`);
+                }
+                else {
+                    console.log(`Not Checked ${i}: ${t1[i].value}`);
+                    t2[i].style.textDecoration = "none";
+                    t2[i].style.backgroundColor = "lightgray";
+                    arrListDone.splice(i, 1);
+                    console.log(arrListDone);
+                }
             }
         }
-    }
-    }  
+        }  
 }
 
 // Move task to done list
