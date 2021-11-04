4c39b0d880d4d08090cc7530b7890aac02292f78
25797700+madaskalas@users.noreply.github.com <25797700+madaskalas@users.noreply.github.com>
2021-11-04 20:50:05 +0200

    Add comments

--- main.js
+++ main.js
@@ -25,19 +25,22 @@ function tasklist() {
     // Fetch value of input text
     task = document.getElementById("userinput").value;
     let namelist = document.getElementById("namelist").value;
-    // console.log(`Name of list: ${namelist}`);
 
+    // Display name of list
     let todolistname = document.querySelector(".todo").innerHTML = namelist;
 
+    // If input fields are empty
     if (task == "" || namelist == "") {
         document.getElementById("warning").innerHTML = "Please complete both fields!";
         document.getElementById("warning").style.display = "block";
         
     }
+    // If input fields are not empty
     else {
         document.getElementById("warning").style.display = "none";
         document.getElementById("namelist").disabled = true;
         document.getElementById("namelist").title = "🚫 Disabled!";
+
         // Select DIV
         let todolist = document.querySelector(".todolist");
 
@@ -50,7 +53,7 @@ function tasklist() {
         // Create input element
         let input = document.createElement("input");
 
-        // Generates a random number
+        // Generates a random number for id
         let random = Math.floor(Math.random() * 1000);
         let randomlabel = Math.floor(Math.random() * 1001 + 1000);
         let randombr = Math.floor(Math.random() * 100);
@@ -96,53 +99,48 @@ function tasklist() {
 
         // Push to array
         arrList.push(task);
-        // console.log(arrList);
 
+        // Select element based on class + id
         let t1 = document.querySelectorAll(".checkclass");
         let t2 = document.querySelectorAll(".labelclass");
         let t3 = document.getElementById(randombr);
         let t4 = document.getElementById(btnrem);
-        // console.log(t1);
-        // console.log(t2);
-        // console.log(t4);
 
+        // If remove btn clicked
         t4.onclick = () => {
             console.log(`Button clicked! ${btnrem}`);
+
+            // Select <br> with id
             let parent = document.getElementById(randombr);
             let varbr =  parent.getElementsByTagName("br");
+
+            // Remove <br> with id
             parent.parentElement.removeChild(parent);
 
             let randomrem = document.getElementById(random);
             let randomlblrem = document.getElementById(randomlabel);
-            // randomrem.parentElement.removeChild(randomrem);
-            // randomlblrem.parentElement.removeChild(randomlblrem);
-            // t4.parentElement.removeChild(t4);
+
+            // Hide items on click remove button
             randomrem.style.display = "none";
             randomlblrem.style.display = "none";
             t4.style.display = "none";
         }
 
+        // Loop through list array
         for (let i = 0; i <= arrList.length - 1; i++) {
+
+            // If checkbox is checked
             t1[i].onclick = () => {
                 if (t1[i].checked) {
                     console.log(`Checked ${i}: ${t1[i].value}`);
                     t2[i].style.textDecoration = "line-through";
                     t2[i].style.backgroundColor = "lightgreen";
-                    /*
-                    arrListDone.push(arrList[i]);
-                    console.log(`Push to arrListDone`);
-                    console.log(arrListDone);
-                    console.log(`EndIf: ${i}`);
-                    */
                 }
+            // If checkbox is not checked
                 else {
                     console.log(`Not Checked ${i}: ${t1[i].value}`);
                     t2[i].style.textDecoration = "none";
                     t2[i].style.backgroundColor = "lightgray";
-                    /*
-                    arrListDone.splice(i, 1);
-                    console.log(arrListDone);
-                    */
                 }
             }
         }
